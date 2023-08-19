5196fbd900d7dc9ad974baec534316a54b149e14
Shashankss1205 <shashankshekharsingh1205@gmail.com>
2023-08-19 19:19:30 +0530

    first commit

---
+++ lib/UI/MyHomePage.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'package:notes/UI/widgets/CreateNote.dart';
+import 'package:notes/UI/widgets/NoteCard.dart';
+import 'package:notes/models/note_model.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  List<Note> notes = List.empty(growable: true);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.black87,
+        leading:
+          Container(
+            padding: const EdgeInsets.only(left: 10),
+            child: const CircleAvatar(
+            backgroundImage: AssetImage('Images/profile.png'),
+        ),
+          ),
+        title: const Text("My Notes App", style: TextStyle(fontSize: 30 , fontWeight: FontWeight.bold,) ),
+      ),
+
+      body: ListView.builder(
+        itemCount: notes.length,
+        itemBuilder: (context, index){
+          return NoteCard(note: notes[index], index: index, onNoteDeleted:onNoteDeleted);
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.of(context).push(MaterialPageRoute(builder: (context)=> CreateNote(onNewNoteCreated: onNewNoteCreated,)));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void onNewNoteCreated(Note note){
+    notes.add(note);
+    setState(() {});
+  }
+
+  void onNoteDeleted(int index){
+    notes.removeAt(index);
+    setState(() {});
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'UI/MyHomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter SDG',
+      theme: ThemeData(
+          brightness: Brightness.dark
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+
+
+
